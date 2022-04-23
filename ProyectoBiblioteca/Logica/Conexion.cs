f02e6459ad4b0073b551f98cb576cbe568d0a2c2
Enmanuelle Martinez <98730886+EMLDeveloper@users.noreply.github.com>
2022-04-23 02:46:41 -0700

    Proyecto Final de Biblioteca

---
+++ ProyectoBiblioteca/Logica/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace ProyectoBiblioteca.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=DESKTOP-PQ1G2OV\\SQLEXPRESS;Initial Catalog=DB_BIBLIOTECA;Integrated Security=True";
+    }
+}
